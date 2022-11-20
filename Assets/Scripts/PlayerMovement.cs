d56da2a79b772c8d881554f7e7f949cd87f6685c
EnricPa77 <Enricpa77@gmail.com>
2022-11-20 17:06:26 +0100

    Movimiento

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,13 +7,16 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D Rigidbody2D;
     private Animator Animator;
     private float Horizontal;
-    private float Speed;
-    private float JumpForce = 150;
+    public float Speed;
+    public float JumpForce;
+    private bool Grounded;
 
     void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
         Animator = GetComponent<Animator>();
+        Speed = 1;
+        JumpForce = 150;
     }
 
     // Update is called once per frame
@@ -21,18 +24,27 @@ public class PlayerMovement : MonoBehaviour
     {
         Horizontal = Input.GetAxisRaw("Horizontal");
 
+
         if (Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
         else if (Horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
 
         Animator.SetBool("running", Horizontal != 0.0f);
         
-        if(Input.GetKeyDown(KeyCode.Space)){
+        if (Physics2D.Raycast(transform.position, Vector3.down, 0.1f)) { 
+            Grounded = true;
+        }
+
+        else {
+            Grounded = false;
+        }
+        
+        if(Input.GetKeyDown(KeyCode.Space) && Grounded){
             Jump();
         }
     }
 
     private void FixedUpdate(){
-        Rigidbody2D.velocity = new Vector2(Horizontal, Rigidbody2D.velocity.y);
+        Rigidbody2D.velocity = new Vector2(Horizontal * Speed, Rigidbody2D.velocity.y);
     }
 
     private void Jump(){
